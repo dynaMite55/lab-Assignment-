6cc807699d9fe863e98a3b6e720e4b733cb78692
dynaMite55 <85568488+dynaMite55@users.noreply.github.com>
2021-12-30 17:50:51 +0530

    Lab7
    Uploaded successfullu

---
+++ Lab 7/Lab 7 assignment 1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7_Assignment_1
+{
+    public struct student
+    {
+        private int roll;
+        private string name;
+        private string gen;
+        private double mob;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Roll
+        {
+            get { return this.roll; }
+            set { this.roll = value; }
+        }
+
+        public string Gender
+        {
+            get { return this.gen; }
+            set { this.gen = value; }
+        }
+
+        public double Mob
+        {
+            get { return this.mob; }
+            set { this.mob = value; }
+        }
+
+        public student(int roll1, string name1, string gen1, int mob1)
+        {
+            this.roll = roll1;
+            this.name = name1;
+            this.gen = gen1;
+            this.mob = mob1;
+        }
+
+        public void printDetails()
+        {
+            Console.WriteLine("\nStudent Details\n");
+            Console.WriteLine(".....................................\n");
+            Console.WriteLine($"Roll No    : {this.roll}");
+            Console.WriteLine($"Name       : {this.name}");
+            Console.WriteLine($"Gender     : {this.gen}");
+            Console.WriteLine($"Mobile No  : {this.mob}");
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            student s = new student();
+            Console.Write("Enter the Roll no : ");
+            s.Roll = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Name : ");
+            s.Name = (Console.ReadLine());
+            Console.Write("Enter the Gender : ");
+            s.Gender = (Console.ReadLine());
+            Console.Write("Enter the Mobile no : ");
+            s.Mob = Convert.ToDouble(Console.ReadLine());
+            s.printDetails();
+            Console.ReadLine();
+
+
+        }
+    }
+}
